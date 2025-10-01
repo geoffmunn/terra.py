15d133e6c7c553e4031610f4ee58a2cad74bee7e
Vegas <103571823+vegasmorph@users.noreply.github.com>
2025-10-01 11:02:07 +0100

    Update from_contract_events.py

--- integration_tests/from_contract_events.py
+++ integration_tests/from_contract_events.py
@@ -1,7 +1,7 @@
 from terra_classic_sdk.client.lcd import LCDClient
 from terra_classic_sdk.util.contract import get_contract_events
 
-tequila = LCDClient(url="https://lcd.terra.dev", chain_id="columbus-5")
+tequila = LCDClient(url="https://terra-classic-lcd.publicnode.com", chain_id="columbus-5")
 tx_info = tequila.tx.tx_info(
     "D1E80F27435215FF097141C804EDE51C87718355CA8A6A397CC2346144F19D04"
 )
